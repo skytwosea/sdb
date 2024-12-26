57db616203be60c65530f1420233fb3bc72b6418
skytwosea <barryjaypenner@gmail.com>
2024-12-25 20:57:29 -0500

    first commit: hello, sdb

---
+++ build/src/CMakeFiles/libsdb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libsdb.dir/libsdb.cpp.o"
+  "CMakeFiles/libsdb.dir/libsdb.cpp.o.d"
+  "libsdb.a"
+  "libsdb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libsdb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/libsdb.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for libsdb.
+# This may be replaced when dependencies are built.
