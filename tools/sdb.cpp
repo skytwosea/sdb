0df7ff75f37885b9fdb9be9b79a3387362f7470a
skytwosea <barryjaypenner@gmail.com>
2024-12-28 19:00:19 -0500

    Adds basic UI
    manual testing with `yes` works, but not quite with the while sleep ... snippet on p 36. Continuing through next section and comparing refactored code with official `sdb` repo

--- tools/sdb.cpp
+++ tools/sdb.cpp
@@ -1,7 +1,15 @@
 #include <libsdb/libsdb.hpp>
+#include <iostream>
 #include <unistd.h>
 #include <string_view>
 #include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <editline/readline.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
 
 namespace {
     pid_t attach(int argc, const char** argv) {
@@ -44,6 +52,51 @@ namespace {
     }
 }
 
+namespace {
+    std::vector<std::string> split(std::string_view str, char delimiter) {
+        std::vector<std::string> out{};
+        std::stringstream ss {std::string{str}};
+        std::string item;
+        while (std::getline(ss, item, delimiter)) {
+            out.push_back(item);
+        }
+        return out;
+    }
+
+    bool is_prefix(std::string_view str, std::string_view of) {
+        if (str.size() > of.size()) return false;
+        return std::equal(str.begin(), str.end(), of.begin());
+    }
+
+    void resume(pid_t pid) {
+        if (ptrace(PTRACE_CONT, pid, nullptr, nullptr) < 0) {
+            std::cerr << "Couldn't continue\n";
+            std::exit(-1);
+        }
+    }
+
+    void wait_on_signal(pid_t pid) {
+        int wait_status;
+        int options = 0;
+        if (waitpid(pid, &wait_status, options) < 0) {
+            std::perror("waitpid failed");
+            std::exit(-1);
+        }
+    }
+
+    void handle_command(pid_t pid, std::string_view line) {
+        auto args = split(line, ' ');
+        auto command = args[0];
+        if (is_prefix(command, "continue")) {
+            resume(pid);
+            wait_on_signal(pid);
+        }
+        else {
+            std::cerr << "Unknown command\n";
+        }
+    }
+}
+
 int main(int argc, const char** argv) {
     if (argc == 1) {
         std::cerr << "No arguments given\n";
@@ -54,5 +107,24 @@ int main(int argc, const char** argv) {
     int options = 0;
     if (waitpid(pid, &wait_status, options) < 0) {
         std::perror("waitpid failed");
+    }
+
+    char* line = nullptr;
+    while ((line = readline("sdb> ")) != nullptr) {
+        std::string line_str;
+        if (line == std::string_view("")) {
+            free(line);
+            if (history_length > 0) {
+                line_str = history_list()[history_length - 1]->line;
+            }
+        }
+        else {
+            line_str = line;
+            add_history(line);
+            free(line);
+        }
+        if (!line_str.empty()) {
+            handle_command(pid, line_str);
+        }
     }
 }
